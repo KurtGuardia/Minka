0d80d266b6c37eaa51e53ebfce16d824c60654fe
Kurt Guardia <kaguardiagamarra@wideagency.com>
2024-08-26 22:43:25 +0200

    init: Initial poject setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import { join } from 'path'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  sassOptions: {
+    includePaths: [
+      join(
+        dirname(fileURLToPath(import.meta.url)),
+        'src/styles',
+      ),
+    ],
+    prependData: `@import "globals.scss";`,
+  },
+}
+
+export default nextConfig
