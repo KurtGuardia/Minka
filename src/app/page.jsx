3b16218e2a82e36b26926a0a5b1544c20b6e18c7
Kurt Guardia <kaguardiagamarra@wideagency.com>
2024-08-27 22:19:45 +0200

    feat: Navbar done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,5 @@
 import styles from './page.module.scss'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1 className={styles.heading}>Que opinan</h1>
-      <h2 className={styles.headingSec}>Hello Minka</h2>
-      <p className={styles.textAlt}>Test</p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing
-        elit. Et harum quo dolores repellendus unde.
-        Distinctio repellat voluptatem deserunt autem ab
-        quasi reiciendis fuga modi voluptate ex nemo animi,
-        doloribus nulla.
-      </p>
-      <button className={styles.buttonPrimary}>Try me</button>
-      <button className={styles.buttonSecondary}>test me</button>
-    </main>
-  )
+  return <main className={styles.main}></main>
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from './Navbar.module.scss'
+
+const Navbar = () => {
+  const [isSticky, setIsSticky] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSticky(true)
+      } else {
+        setIsSticky(false)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () =>
+      window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <header
+      className={`${styles.header} ${
+        isSticky ? styles.sticky : ''
+      }`}
+    >
+      <div className={styles.container}>
+        <Link href='/'>
+          <Image
+            src='/logo.svg'
+            alt='Minka Logo'
+            width={150}
+            height={50}
+          />
+        </Link>
+        <nav className={styles.nav}>
+          <ul className={styles.ul}>
+            <li className={styles.li}>
+              <Link href='/recaudarfondos'>
+                Recaudar fondos
+              </Link>
+            </li>
+            <li className={styles.li}>
+              <Link href='/donar'>Donar</Link>
+            </li>
+            <li className={styles.li}>
+              <Link href='/ingresar' className={styles.buttonWhite}>Ingresar</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
