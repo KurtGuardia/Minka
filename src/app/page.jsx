d107c3b95b2ea753d9d3dfb77f2a58ef49d0e9aa
Kurt Guardia <kaguardiagamarra@wideagency.com>
2024-08-31 18:58:59 +0200

    fix: Fix font-size in Not Responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,11 @@ import Offer from '@/components/Offer/Offer'
 import Reviews from '@/components/Reviews/Reviews'
 import FAQ from '@/components/FAQ/FAQ'
 import Contact from '@/components/Contact/Contact'
-import Footer from '@/components/Footer/Footer'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.heading}>
+      <h1 className={styles.headingSec}>
         NOT YET RESPONSIVE. Check it out on desktop, thank
         you.
       </h1>
